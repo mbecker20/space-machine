2f0a2012998994e4a0058506c92fa0ed9be514f6
mbecker20 <becker.maxh@gmail.com>
2020-06-20 18:33:01 -0700

    set up redux chrome devtools

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import moduleReducer from './module/moduleReducer'
 
-const store = createStore(moduleReducer)
+const store = createStore(moduleReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store
