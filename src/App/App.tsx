5e41adbe525d5c74008c7532d6430b880cdcc3e7
Max <becker.maxh@gmail.com>
2020-06-24 01:51:33 -0700

    app runs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import useJSS from './style'
 import { Drawer, ModuleViewFill } from '../components/all'
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/stateTSTypes'
 
 function App() {
   const classes = useJSS()
+  const { fillContainerID, containerModules } = useSelector((state: RootState) => {
+    return {
+      fillContainerID: state.fillContainerID,
+      containerModules: state.containerModules,
+    }
+  })
   return (
     <div className={classes.Bounder}>
       <Drawer />
-      <ModuleViewFill />
+      <ModuleViewFill mod={containerModules[fillContainerID]}/>
     </div>
   )
 }
