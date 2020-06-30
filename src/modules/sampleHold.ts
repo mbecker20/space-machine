cbc3316d7af08c010acd1850731755cb59f667e9
Karamvir Singh <karamvirsingh@Karamvirs-MacBook-Pro.local>
2020-06-29 17:28:29 -0700

    sampleHold v1

---
+++ src/modules/sampleHold.ts
@@ -0,0 +1,33 @@
+import audioCtx from '../audioCtx'
+
+/*
+function makeSampleHold(input:number, hold: number, output: number) {
+  const sampleHold = audioCtx.createConstantSource()
+  let isHolding = false
+  
+  function setState(newIsHolding: boolean) {
+    isHolding = newIsHolding
+  }
+
+  function holding (){
+    if (isHolding) {
+      //output = held value
+    } else {
+      //output = input 
+    }
+  }
+
+}
+*/
+
+function makeSampleHold(inputModule: AudioNode) {
+  const sampleHold = audioCtx.createOscillator()
+/*
+  function sampleInput(){
+    inputModule. = sampleHold.frequency
+  }
+  */
+  function holdSample() {
+    sampleHold.frequency.cancelAndHoldAtTime(audioCtx.currentTime)
+  }
+}
