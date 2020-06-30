fbb274817f43f29aa8945561cef9386b95d62cb4
Max Becker <becker.maxh@gmail.com>
2020-06-29 17:58:00 -0700

    commit

--- src/components/TestingArea/TestingArea.tsx
+++ src/components/TestingArea/TestingArea.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import useJSS from './style'
-//import audioCtx from '../../audioCtx'
+import audioCtx from '../../audioCtx'
 import { makeOscillator, makeSampleHold } from '../../modules/all'
 
+const sampleOsc = makeOscillator('sine', 440, 0)
 
+const sampleHold = makeSampleHold(sampleOsc.audioNode)
+
+sampleHold.audioNode.connect(audioCtx.destination)
 
 function TestingArea() {
   const classes = useJSS()
@@ -11,12 +15,28 @@ function TestingArea() {
     <div className={classes.TestingArea}>
       {'testing area'}
       <div className={classes.ButtonDiv}>
+      <div className={classes.ButtonDiv}>
+        <div className={classes.Button}
+          style={{ backgroundColor: 'green' }}
+          onClick={() => {
+            audioCtx.resume()
+          }}
+        >start ctx</div>
+      </div>
         <div className={classes.Button}
           style={{ backgroundColor: 'blue' }}
           onClick={() => {
-
+            sampleHold.holdSample()
           }}
         >button</div>
+      </div>
+      <div className={classes.ButtonDiv}>
+        <div className={classes.Button}
+          style={{ backgroundColor: 'red' }}
+          onClick={() => {
+            audioCtx.suspend()
+          }}
+        >stop ctx</div>
       </div>
     </div>
   );
