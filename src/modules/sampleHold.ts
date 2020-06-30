6f2a2546f944ac9d3a2243995d5f1dc871f5beb1
Karamvir Singh <karamvirsingh@Karamvirs-MacBook-Pro.local>
2020-06-29 18:02:35 -0700

    commit

--- src/modules/sampleHold.ts
+++ src/modules/sampleHold.ts
@@ -2,7 +2,6 @@ import audioCtx from '../audioCtx'
 
 function makeSampleHold(inputModule: AudioNode) {
   const sampleHold = audioCtx.createOscillator()
-  sampleHold.frequency.value = .5
   inputModule.connect(sampleHold.frequency)
   function holdSample() {
     sampleHold.frequency.cancelAndHoldAtTime(audioCtx.currentTime)
