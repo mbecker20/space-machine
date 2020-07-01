109e52b5ff6b1dfdab7444ef6def34abca284b7b
Max Becker <becker.maxh@gmail.com>
2020-07-01 15:23:52 -0700

    update centermenu to darken everything in background

--- src/components/CenterMenu/CenterMenu.tsx
+++ src/components/CenterMenu/CenterMenu.tsx
@@ -10,15 +10,22 @@ interface Props {
 function CenterMenu({ children, header, onClose }: Props) {
   const classes = useJSS()
   return (
-    <div className={classes.CenterMenu} onKeyUp={(e) => {
-      if (e.keyCode === 27 ) { //escape
-        onClose()
-      }
-    }}>
-      <div className={classes.Header}>
-        {header}
+    <div className={classes.CenterMenuBounder}
+      onClick={onClose}
+    >
+      <div className={classes.CenterMenu}
+        onKeyUp={(e) => {
+          if (e.keyCode === 27 ) { //escape
+            onClose()
+          }
+        }}
+        onClick={(e) => {e.stopPropagation()}}
+      >
+        <div className={classes.Header}>
+          {header}
+        </div>
+        {children}
       </div>
-      {children}
     </div>
   )
 }
