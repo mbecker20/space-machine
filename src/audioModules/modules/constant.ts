4a1d439acdf69e14827762e8e6ac5f6203c08f0f
Karamvir Singh <karamvirsingh@Karamvirs-MacBook-Pro.local>
2020-07-04 15:30:07 -0700

    fix

--- src/audioModules/modules/constant.ts
+++ src/audioModules/modules/constant.ts
@@ -1,33 +1,37 @@
 import audioCtx from '../../audioCtx'
-import { BaseAM, BaseControls } from '../moduleTypes'
+import { BaseAM, BaseControls, VALUE } from '../moduleTypes'
 
 export interface ConstantModule extends BaseAM {
   audioNode: ConstantSourceNode
   controls: BaseControls
 }
 
-function makeConstant(value = 0) {
+function makeConstant(value = 0): ConstantModule {
 
   const constant = audioCtx.createConstantSource()
   constant.offset.setValueAtTime(value, audioCtx.currentTime)
 
-  function setValue(newValue: number) {
-    constant.offset.setValueAtTime(newValue, audioCtx.currentTime)
+  function setValue(newValue: string) {
+    constant.offset.setValueAtTime(Number(newValue), audioCtx.currentTime)
   }
 
-  function linRampValue(newValue: number) {
-    constant.offset.linearRampToValueAtTime(newValue, audioCtx.currentTime)
+  function linRampValue(newValue: string) {
+    constant.offset.linearRampToValueAtTime(Number(newValue), audioCtx.currentTime)
   }
 
-  function expRampValue(newValue: number) {
-    constant.offset.exponentialRampToValueAtTime(newValue, audioCtx.currentTime)
+  function expRampValue(newValue: string) {
+    constant.offset.exponentialRampToValueAtTime(Number(newValue), audioCtx.currentTime)
   }
 
   const controls = {
-    'value': value,
+    'set value': setValue,
   }
   
-  return { audioNode: constant, paramIDs: [value] }
+  return { 
+    audioNode: constant, 
+    paramIDs: [['value', VALUE], ],
+    controls
+  }
 
 }
 
