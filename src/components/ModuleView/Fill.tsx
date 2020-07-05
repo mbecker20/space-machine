86ae1c557b9ecd5fb5593b21f5243e74c5160c52
Max Becker <becker.maxh@gmail.com>
2020-07-04 22:33:40 -0700

    add archer container to fill

--- src/components/ModuleView/Fill.tsx
+++ src/components/ModuleView/Fill.tsx
@@ -3,13 +3,13 @@ import useJSS from './style'
 import CSS from 'csstype'
 import { sizes } from '../../theme/theme'
 import ModuleViewIcon from './Icon'
-//import ModuleViewIcon from './Icon'
 import { ContainerModule } from '../../redux/stateTSTypes'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../redux/stateTSTypes'
 import { getGridRange } from './helpers'
 import { range } from '../../helpers/genFuncs'
 import DropSquare from './DropSquare'
+import { ArcherContainer } from 'react-archer'
 
 declare global {
   interface Window { setFillIsExpanded: (isExpanded: boolean) => void }
@@ -55,37 +55,39 @@ function ModuleViewFill({ containerModule }: Props) {
           window.setLeftDrawerOpen(true)
         }}
       >{containerModule.name}</div>
-      <div className={classes.Fill} style={gridStyle} onClick={() => {
-        window.highlightedID = ''
-        window.currSetHighlighted(false)
-        window.currSetHighlighted = (setHighlighted) => {}
-        window.setLeftDrawerOpen(false)
-      }}>
-        {!isExpanded ? null : 
-        isEmpty ? <DropSquare row={0} col={0}/> :
-        range(0, isExpanded ? maxRow + 2 : maxRow + 1).map(row => {
-          return range(0, isExpanded ? maxCol + 2 : maxCol + 1).map(col => {
+      <ArcherContainer>
+        <div className={classes.Fill} style={gridStyle} onClick={() => {
+          window.highlightedID = ''
+          window.currSetHighlighted(false)
+          window.currSetHighlighted = (setHighlighted) => {}
+          window.setLeftDrawerOpen(false)
+        }}>
+          {!isExpanded ? null : 
+          isEmpty ? <DropSquare row={0} col={0}/> :
+          range(0, isExpanded ? maxRow + 2 : maxRow + 1).map(row => {
+            return range(0, isExpanded ? maxCol + 2 : maxCol + 1).map(col => {
+              return (
+                <DropSquare
+                  key={`${row} ${col}`}
+                  row={row} 
+                  col={col}
+                />
+              )
+            })
+          }).flat()}
+          {containerModule.childModules.map(moduleID => {
+            const mod = modules[moduleID]
             return (
-              <DropSquare
-                key={`${row} ${col}`}
-                row={row} 
-                col={col}
+              <ModuleViewIcon
+                key={mod.id}
+                mod={mod}
+                gridRow={mod.row * 2 + 1}
+                gridCol={mod.col * 2 + 1}
               />
             )
-          })
-        }).flat()}
-        {containerModule.childModules.map(moduleID => {
-          const mod = modules[moduleID]
-          return (
-            <ModuleViewIcon
-              key={mod.id}
-              mod={mod}
-              gridRow={mod.row * 2 + 1}
-              gridCol={mod.col * 2 + 1}
-            />
-          )
-        })}
-      </div>
+          })}
+        </div>
+      </ArcherContainer>
     </div>
   )
 }
