f4dbb1ba21f5253ebc3675e6fa69c24775f1afbd
mbecker20 <becker.maxh@gmail.com>
2020-07-07 16:27:55 -0700

    new system

--- src/audioModules/modules/kompressor.ts
+++ src/audioModules/modules/kompressor.ts
@@ -13,47 +13,32 @@ function makeKompressor(): KompressorModule {
   const controlData: ControlData = {
     'set threshold': {
       controlType: VALUE,
-      value: kompressor.threshold.value
+      paramID: 'threshold',
     },
     'set knee': {
       controlType: VALUE,
-      value: kompressor.knee.value
+      paramID: 'knee'
     },
     'set ratio': {
       controlType: VALUE,
-      value: kompressor.ratio.value
+      paramID: 'ratio'
     },
     'set attack': {
       controlType: VALUE,
-      value: kompressor.attack.value
+      paramID: 'attack'
     },
     'set release': {
       controlType: VALUE,
-      value: kompressor.release.value
+      paramID: 'release'
     }
   }
 
   const controlSetFuncs: ControlSetFuncs = {
-    'set threshold': (newThreshold: string) => {
-      controlData['set threshold'].value = Number(newThreshold)
-      kompressor.threshold.value = controlData['set threshold'].value
-    },
-    'set knee': (newKnee: string) => {
-      controlData['set knee'].value = Number(newKnee)
-      kompressor.knee.value = controlData['set knee'].value
-    },
-    'set ratio': (newRatio: string) => {
-      controlData['set ratio'].value = Number(newRatio)
-      kompressor.ratio.value = controlData['set ratio'].value
-    },
-    'set attack': (newAttack: string) => {
-      controlData['set attack'].value = Number(newAttack)
-      kompressor.attack.value = controlData['set attack'].value
-    },
-    'set release': (newRelease: string) => {
-      controlData['set release'].value = Number(newRelease)
-      kompressor.release.value = controlData['set release'].value
-    },
+    'set threshold': (newThreshold: string) => { kompressor.threshold.value = Number(newThreshold) },
+    'set knee': (newKnee: string) => { kompressor.knee.value = Number(newKnee) },
+    'set ratio': (newRatio: string) => { kompressor.ratio.value = Number(newRatio) },
+    'set attack': (newAttack: string) => { kompressor.attack.value = Number(newAttack)},
+    'set release': (newRelease: string) => { kompressor.release.value = Number(newRelease) },
   }
 
   return {
