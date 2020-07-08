9cfeca18f73ae41a5ccf35310c9e4bb9667e9d1e
Karamvir Singh <karamvirsingh@Karamvirs-MacBook-Pro.local>
2020-07-07 17:46:11 -0700

    osc update

--- src/audioModules/modules/oscillator.ts
+++ src/audioModules/modules/oscillator.ts
@@ -1,33 +1,45 @@
 import audioCtx from '../../audioCtx'
-import { BaseAM, BaseControls, TYPE, VALUE, BUTTON } from '../moduleTypes'
+import { BaseAM, ControlData, ControlSetFuncs, TYPE, VALUE, BUTTON } from '../moduleTypes'
 
 export interface OscillatorModule extends BaseAM {
   audioNode: OscillatorNode
-  controls: BaseControls
   typeTypes: string[]
 }
 
-const oscTypes = ['sine', 'square', 'triangle', 'sawtooth']
+const oscillatorTypes = ['sine', 'square', 'triangle', 'sawtooth']
 
 function makeOscillator(): OscillatorModule {
- 
-  const osc = audioCtx.createOscillator()
+  const oscillator = audioCtx.createOscillator()
    
-  const controls = {
-    'set type': (newType: string) => {osc.type = newType as OscillatorType},
-    'set frequency': (newFrequency: string) => {osc.frequency.value = Number(newFrequency)},
-    'set detune': (newDetune: string) => {osc.detune.value = Number(newDetune)},
-    'kill': (arg = '') => {osc.stop()}
+  const connectingParamIDs = ['frequency', 'detune']
+
+  const controlData: ControlData = {
+    'set frequency': {
+      controlType: VALUE,
+      paramID: 'frequency'
+    },
+    'set detune': {
+      controlType: VALUE,
+      paramID: 'detune'
+    }
   }
 
-  osc.start()
+  const controlSetFuncs: ControlSetFuncs = {
+    'set frequency': (newFrequency: string) => { oscillator.frequency.value = Number(newFrequency) },
+    'set detune': (newDetune: string) => { oscillator.detune.value = Number(newDetune) },
+    'kill': (arg = '') => { oscillator.stop() }
+  }
   
   return { 
-    audioNode: osc,
-    paramIDs: [['type', TYPE], ['frequency', VALUE], ['detune', VALUE], ['kill', BUTTON]],
-    typeTypes: oscTypes,
-    controls,
+    audioNode: oscillator,
+    typeTypes: oscillatorTypes,
+    connectingParamIDs,
+    controlData,
+    controlSetFuncs,
   }
+
+  oscillator.start()
+
 }
 
 export default makeOscillator
