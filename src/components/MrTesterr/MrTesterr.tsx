0789e3954c888372fccc471eabf73dbbe2a0a22b
mbecker20 <becker.maxh@gmail.com>
2020-07-19 03:42:19 -0700

    new effect

--- src/components/MrTesterr/MrTesterr.tsx
+++ src/components/MrTesterr/MrTesterr.tsx
@@ -2,18 +2,14 @@ import React from 'react'
 import useJSS from './style'
 import audioCtx from '../../audioCtx'
 
-interface Thing {
-  audioNode?: MediaStreamAudioSourceNode
-}
-
-const thing: Thing = {}
-
+/*
 var splitter = audioCtx.createChannelSplitter(2)
-var merger = audioCtx.createChannelMerger(2)
+
 var lpf = audioCtx.createBiquadFilter()
   lpf.type = "lowpass"
   lpf.frequency.value = 440
   lpf.Q.value = 10
+*/
 
 navigator.mediaDevices.getUserMedia({
   audio: {
@@ -23,14 +19,23 @@ navigator.mediaDevices.getUserMedia({
     latency: 0
   }
 }).then(stream => {
-  thing.audioNode = audioCtx.createMediaStreamSource(stream)
-})
+  const src = audioCtx.createMediaStreamSource(stream)
+  const gain = audioCtx.createGain()
+  const merger = audioCtx.createChannelMerger(2)
+  const dly = audioCtx.createDelay(); dly.delayTime.value = .18
+  const dlyFdbk = audioCtx.createGain(); dlyFdbk.gain.value = .6
+  dly.connect(dlyFdbk); dlyFdbk.connect(dly)
+  const master = audioCtx.createGain(); master.gain.value = 1
 
-thing.lineInSource.connect(splitter)
-splitter.connect(lpf, 1)
-lpf.connect(merger, 0, 1)
-splitter.connect(merger, 0, 0)
-merger.connect(audioCtx.destination)
+  gain.gain.value = 5.5
+  src.connect(gain, 0, 0)
+  gain.connect(merger, 0, 0)
+  gain.connect(merger, 0, 1)
+  merger.connect(dly)
+  dly.connect(master)
+  merger.connect(master)
+  master.connect(audioCtx.destination)
+})
 
 function MrTesterr() {
   const classes = useJSS()
@@ -44,32 +49,6 @@ function MrTesterr() {
             audioCtx.resume()
           }}
         >start ctx</div>
-      </div>
-      <div className={classes.ButtonDiv}>
-        <div className={classes.Button}
-          style={{ backgroundColor: 'blue' }}
-        >
-          {'osc freq'}
-          <input style={{ height: '2em' }}
-            
-            onChange={(e) => {
-              //osc.controls['set frequency'](e.target.value)
-            }}
-          />
-        </div>
-      </div>
-      <div className={classes.ButtonDiv}>
-        <div className={classes.Button}
-          style={{ backgroundColor: 'blue' }}
-        >
-          {'lpf freq'}
-          <input style={{ height: '2em' }}
-            
-            onChange={(e) => {
-              //lpf.controls['set frequency'](e.target.value)
-            }}
-          />
-        </div>
       </div>
       <div className={classes.ButtonDiv}>
         <div className={classes.Button}
