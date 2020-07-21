f6484819908f729a380766898e7c34cda401c3cf
mbecker20 <becker.maxh@gmail.com>
2020-07-21 03:15:14 -0700

    Updates

--- precache-manifest.867efb4915d9553c4538055ce7ecf6ad.js
+++ precache-manifest.867efb4915d9553c4538055ce7ecf6ad.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97627dbd4401aca8c39525f4fa73dea1",
+    "revision": "6f800e416b6a6b121009b281aeea6195",
     "url": "/space-machine/index.html"
   },
   {
-    "revision": "e62a6d6a5654c51c2bd4",
+    "revision": "e5e8408641600629857c",
     "url": "/space-machine/static/css/main.5ff894c0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/space-machine/static/js/2.10d7d7c5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e62a6d6a5654c51c2bd4",
-    "url": "/space-machine/static/js/main.2ce46f3a.chunk.js"
+    "revision": "e5e8408641600629857c",
+    "url": "/space-machine/static/js/main.668879fe.chunk.js"
   },
   {
     "revision": "3b767ef35ca3650f6e49",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/space-machine/precache-manifest.933b518835fe35eb01a380e94212380b.js"
+  "/space-machine/precache-manifest.867efb4915d9553c4538055ce7ecf6ad.js"
 );
 
 self.addEventListener('message', (event) => {
