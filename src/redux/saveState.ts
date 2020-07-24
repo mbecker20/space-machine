9c49396f3f5349e3bcd3cabec210d8fb3e397a4c
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-24 03:20:55 -0700

    small

--- src/redux/saveState.ts
+++ src/redux/saveState.ts
@@ -1,7 +1,9 @@
+import { RootState } from "./stateTSTypes";
+
 export function saveState() {
 
 }
 
-export function restoreFromState() {
+export function restoreFromState(state: RootState) {
   
 }
