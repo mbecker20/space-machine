8aa5e2d8891ccb8524c93f09ab7761f8bb164cbb
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-31 15:15:54 -0700

    add basic rethink db integration

--- src/components/RightDrawer/FileMenu.tsx
+++ src/components/RightDrawer/FileMenu.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect, db, Connection } from 'rethinkdb'
+import { Button } from '../all'
+import { RootState } from '../../redux/stateTSTypes'
+
+declare global {
+  interface Window {
+    spaceDBConnection: Connection
+  }
+}
+
+export interface Save {
+  id: string,
+  savedState: RootState,
+}
+
+connect({ host: 'localhost', port: 28015 }, (err, connection) => {
+  if (err) throw err
+  window.spaceDBConnection = connection
+})
+
 
 function FileMenu() {
+  const [ saveList, setSaveList ] = useState<string[]>([])
+  if (window.spaceDBConnection) {
+    db('spaceDB').table('saves').map(save => save('id')).run(window.spaceDBConnection, (err, realSaveList) => {
+      if (err) throw err
+      realSaveList.toArray().then(sl => { setSaveList(sl) })
+    })
+  }
   return (
     <div>
-      
+      <div>
+        {saveList.map(saveName => {
+          return (
+            <div key={saveName}>
+              {saveName}
+            </div>
+          )
+        })}
+      </div>
+      <Button
+        onClick={() => {
+          window.openSaveMenu(saveList)
+        }}
+      >
+        save project
+      </Button>
     </div>
   )
 }
