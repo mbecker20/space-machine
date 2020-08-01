bf734b7c9f467af33bcd39df0060613763f9b057
Max Becker <max@Maxs-MacBook-Pro.local>
2020-07-31 20:43:49 -0700

    remove react-rethinkdb (kind of outdated)

--- src/components/RightDrawer/FileMenu.tsx
+++ src/components/RightDrawer/FileMenu.tsx
@@ -1,17 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from '../all'
 import { RootState } from '../../redux/stateTSTypes'
-var ReactRethinkdb = require('react-rethinkdb')
-var r = ReactRethinkdb.r
-
-ReactRethinkdb.DefaultSession.connect({
-  host: 'localhost',          // hostname of the websocket server
-  port: 8015,                 // port number of the websocket server
-  path: '/',                  // HTTP path to websocket route
-  secure: false,              // set true to use secure TLS websockets
-  db: 'test',                 // default database, passed to rethinkdb.connect
-  autoReconnectDelayMs: 2000, // when disconnected, millis to wait before reconnect
-})
 
 declare global {
   interface Window {
