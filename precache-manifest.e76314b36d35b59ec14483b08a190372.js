395a3ab60c444ed6d004d770a944e9139723624e
Max Becker <max@Maxs-MacBook-Pro.local>
2020-08-03 12:12:52 -0700

    Updates

---
+++ precache-manifest.e76314b36d35b59ec14483b08a190372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1231818c2c419ca03b261a182d17faa0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dfe95070f6a4b5f9e28b",
+    "url": "./static/css/main.d776b7e9.chunk.css"
+  },
+  {
+    "revision": "2487fd404f513a3922bc",
+    "url": "./static/js/2.3f65c9c2.chunk.js"
+  },
+  {
+    "revision": "c881ea73dbcdc325602c5cb3ddd4fe98",
+    "url": "./static/js/2.3f65c9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe95070f6a4b5f9e28b",
+    "url": "./static/js/main.2ebe6349.chunk.js"
+  },
+  {
+    "revision": "07b18276223127fe0310",
+    "url": "./static/js/runtime-main.4bf2a6ae.js"
+  },
+  {
+    "revision": "04e848f8f484417eea06b36b6874e01b",
+    "url": "./static/media/settings.04e848f8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/space-machine/precache-manifest.2fe1fa8c1cc308edf940398feeca078b.js"
+  "./precache-manifest.e76314b36d35b59ec14483b08a190372.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/space-machine/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
