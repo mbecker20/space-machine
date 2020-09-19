2000b3b1e47b55d1bce486ef9b979951d6b18b2c
mbecker20 <becker.maxh@gmail.com>
2020-09-19 04:03:31 -0700

    start making a context menu component

---
+++ src/components/ContextMenus/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContextMenu() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContextMenu
---
+++ src/components/ContextMenus/ContextMenus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContextMenus() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContextMenus
