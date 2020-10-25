9ef9e09bdda3503eb0e0d4d68b5f5733379827e6
mbecker20 <becker.maxh@gmail.com>
2020-10-24 18:00:27 -0700

    remove fragment import

--- src/components/ContextMenus/Module/FileSaveButton.tsx
+++ src/components/ContextMenus/Module/FileSaveButton.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { CONTAINER } from '../../../audioModules/moduleTypes'
 import { RootState } from '../../../redux/stateTSTypes'
@@ -15,7 +15,7 @@ function FileSaveButton({ modID }: Props) {
     <Conditional showIf={modules[modID].moduleType === CONTAINER}>
       <Button
         onClick={() => {
-          
+
         }}
       >
         save to file
